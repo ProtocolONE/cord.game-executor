c8721fd905500943746e191562b34ce54d147a16
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2012-07-10 12:02:04 +0000

    Approve the hook test.

--- GameExecutor/i18n/GameExecutor_ru.ts
+++ GameExecutor/i18n/GameExecutor_ru.ts
@@ -7,17 +7,17 @@
         <location filename="../src/GameExecutor/Hook/DisableDEP.cpp" line="30"/>
         <location filename="../src/GameExecutor/Hook/DisableIEDefalutProxy.cpp" line="37"/>
         <source>TITLE_ATTENTION</source>
-        <translation type="unfinished">Внимание</translation>
+        <translation>Внимание</translation>
     </message>
     <message>
         <location filename="../src/GameExecutor/Hook/DisableDEP.cpp" line="30"/>
         <source>WARNING_DISABLEDEP</source>
-        <translation type="unfinished">Игра не может быть запущена. В вашей ОС включена система предотвращения выполнения данных (DEP). Необходимо отключить DEP вручную. Если у вас возникнут проблемы с отключением, обратитесь в службу поддержки GameNet: https://support.gamenet.ru/</translation>
+        <translation>Игра не может быть запущена. В вашей ОС включена система предотвращения выполнения данных (DEP). Необходимо отключить DEP вручную. Если у вас возникнут проблемы с отключением, обратитесь в службу поддержки GameNet: https://support.gamenet.ru/</translation>
     </message>
     <message>
         <location filename="../src/GameExecutor/Hook/DisableIEDefalutProxy.cpp" line="38"/>
         <source>WARNING_DISABLE_IE_DEFALUT_PROXY</source>
-        <translation type="unfinished">Во избежание проблем с подключением к игровому серверу не рекомендуется использовать функции \&amp;quot;Автономная работа\&amp;quot; и \&amp;quot;Использовать прокси сервер для подключений LAN\&amp;quot; в браузере Internet Explorer. Вы хотите, чтобы GameNet автоматически отключил эти функции?</translation>
+        <translation>Во избежание проблем с подключением к игровому серверу не рекомендуется использовать функции \&amp;quot;Автономная работа\&amp;quot; и \&amp;quot;Использовать прокси сервер для подключений LAN\&amp;quot; в браузере Internet Explorer. Вы хотите, чтобы GameNet автоматически отключил эти функции?</translation>
     </message>
 </context>
 </TS>

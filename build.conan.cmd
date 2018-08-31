ce3887c171e58ef24f80fd118cc90c34fc701f91
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2018-08-31 14:05:12 +0300

    Change namespaces

---
+++ build.conan.cmd
@@ -0,0 +1,10 @@
+set PackageName=GameExecutor/1.0.0@common/stable
+
+conan install . -pr msvcprofile
+conan create . %PackageName% -pr msvcprofile
+
+conan install . -pr msvcprofiled
+conan create . %PackageName% -pr msvcprofiled
+
+@rem conan test CoreTest Core/1.0.0@common/stable -pr msvcprofile
+conan upload %PackageName% --all -r=p1
